2c2e8504f9e0245cc79b422b27f960b819034f0f
obemauricio <mauricioobey@gmail.com>
2021-03-04 15:25:26 -0400

    v1

---
+++ enumeracion.py
@@ -0,0 +1,10 @@
+target = int(input('Choose an interger: '))
+answer = 0
+
+while answer**2 < target:
+    answer += 1
+    
+if answer**2 == target:
+    print(f'The square root of {target} is {answer}')
+else:
+    print(f'{target} does not have an exact square root')
