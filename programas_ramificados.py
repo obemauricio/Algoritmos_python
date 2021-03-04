f6f3088cfd94ab858fbb74ca299b810741ac3bfc
obemauricio <mauricioobey@gmail.com>
2021-03-04 14:42:28 -0400

    if, elif, else

---
+++ programas_ramificados.py
@@ -0,0 +1,9 @@
+num_1 = int(input('Choose a interger: '))
+num_2 = int(input('Choose other interger: '))
+
+if num_1 > num_2:
+    print('The firs number is greater than the second')
+elif num_1 < num_2:
+    print('The second number is greater than the second')
+else:
+    print('Both numbers are equal')
