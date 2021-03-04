bbfa6a23268e08d18badb22cceb8424b597ed437
obemauricio <mauricioobey@gmail.com>
2021-03-04 14:52:37 -0400

    v2

--- iteraciones.py
+++ iteraciones.py
@@ -1,6 +1,10 @@
-counter = 0
+external_counter = 0
+internal_counter = 0
 
-while counter < 10:
-    print(counter)
-    counter += 1
+while external_counter < 5:
+    while internal_counter < 6: #first this lop executed then the next
+        print(external_counter, internal_counter)
+        internal_counter += 1
     
+    external_counter += 1
+    internal_counter = 0
